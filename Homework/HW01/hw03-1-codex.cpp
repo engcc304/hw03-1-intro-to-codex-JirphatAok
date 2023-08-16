65da72f26ff27f2efe34150914b296334604c0fb
Jirphat Saetae <jirphat.aok@gmail.com>
2023-08-16 22:41:01 +0700

    301

--- Homework/HW01/hw03-1-codex.cpp
+++ Homework/HW01/hw03-1-codex.cpp
@@ -6,6 +6,7 @@ int main(){
   int b=20,c=30,d=40;
   printf("This is %d + %d = %d ",a,b,a+b);
   for (int i = 0; i < d - 30; i++){
+    printf( "Here ");
     if ( i==8 ){
       printf("This is 8. ");
     }
